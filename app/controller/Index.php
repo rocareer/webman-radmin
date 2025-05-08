c64d577c14ea4f5955afc84c15756ad9f50b0c78
albert <albert@rocareer.com>
2025-05-09 01:54:37 +0800

    新增控制器基础类BaseController，提供请求处理、中间件和数据验证功能，并新增首页控制器Index和安装向导控制器install
    Signed-off-by: albert <albert@rocareer.com>

---
+++ app/controller/Index.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ *
+ *  * // +----------------------------------------------------------------------
+ *  * // | Rocareer [ ROC YOUR CAREER ]
+ *  * // +----------------------------------------------------------------------
+ *  * // | Copyright (c) 2014~2025 Albert@rocareer.com All rights reserved.
+ *  * // +----------------------------------------------------------------------
+ *  * // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  * // +----------------------------------------------------------------------
+ *  * // | Author: albert <Albert@rocareer.com>
+ *  * // +----------------------------------------------------------------------
+ *
+ */
+
+namespace app\controller;
+
+class Index
+{
+	
+	public function index()
+	{
+        try {
+            if (!is_file(radmin_public() . '/install.lock') && is_file(radmin_app() . '/view/install' . DIRECTORY_SEPARATOR . 'index.html')) {
+                return redirect('/app/radmin/install');
+            }
+            return view('index/index');
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+	
+}
