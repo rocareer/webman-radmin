c03b90b25e7ab9a0b2558f1f80f987d649483a2a
albert <albert@rocareer.com>
2025-05-09 02:25:18 +0800

    feat(plugin/radmin): 新增多语言支持功能 新增 ThinkLang 类实现多语言管理，提供语言包加载、自动侦测、Cookie 存储等功能，并通过 Lang Facade 提供便捷接口。
    Signed-off-by: albert <albert@rocareer.com>

---
+++ support/think/ThinkOrm.php
@@ -0,0 +1,74 @@
+<?php
+/*
+ *
+ *  * // +----------------------------------------------------------------------
+ *  * // | Rocareer [ ROC YOUR CAREER ]
+ *  * // +----------------------------------------------------------------------
+ *  * // | Copyright (c) 2014~2025 Albert@rocareer.com All rights reserved.
+ *  * // +----------------------------------------------------------------------
+ *  * // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  * // +----------------------------------------------------------------------
+ *  * // | Author: albert <Albert@rocareer.com>
+ *  * // +----------------------------------------------------------------------
+ *
+ */
+
+namespace plugin\radmin\support\think;
+
+use support\Cache;
+use support\think\Db;
+use think\Container;
+use think\Paginator;
+use Webman\Bootstrap;
+use Webman\ThinkOrm\DbManager;
+
+class ThinkOrm implements Bootstrap
+{
+	/**
+	 * @var bool
+	 */
+	private static bool $initialized = false;
+	
+	/**
+	 
+	 */
+	public static function start($worker)
+	{
+		if (self::$initialized) {
+			return;
+		}
+		self::$initialized = true;
+		
+		$config = config('think-orm', config('thinkorm'));
+		if (!$config) {
+			return;
+		}
+		
+		Container::getInstance()->bind('think\DbManager', DbManager::class);
+		
+		// 配置
+		Db::setConfig($config);
+		
+		if (class_exists(Cache::class)) {
+			Db::setCache(Cache::store());
+		}
+		
+		Paginator::currentPageResolver(function ($pageName = 'page') {
+			$request = request();
+			if (!$request) {
+				return 1;
+			}
+			$page = $request->input($pageName, 1);
+			if (filter_var($page, FILTER_VALIDATE_INT) !== false && (int)$page >= 1) {
+				return (int)$page;
+			}
+			return 1;
+		});
+		
+		// 设置分页url中域名与参数之间的path字符串
+		Paginator::currentPathResolver(function () {
+			$request = request();
+			return $request ? $request->path() : '/';
+		});
+	}
+}
