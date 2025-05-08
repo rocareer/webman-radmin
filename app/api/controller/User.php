9fe12c91fe7e75a8c53ce0f9f3019d84b6278c8b
albert <albert@rocareer.com>
2025-05-09 01:53:24 +0800

    新增账号管理相关API接口，包含账户概览、资料编辑、验证与绑定、密码修改、积分与余额日志查询及找回密码功能，同时新增Ajax文件上传、图形验证码等通用接口，并支持邮件发送、安装向导等功能
    Signed-off-by: albert <albert@rocareer.com>

---
+++ app/api/controller/User.php
@@ -0,0 +1,126 @@
+<?php
+/*
+ *
+ *  * // +----------------------------------------------------------------------
+ *  * // | Rocareer [ ROC YOUR CAREER ]
+ *  * // +----------------------------------------------------------------------
+ *  * // | Copyright (c) 2014~2025 Albert@rocareer.com All rights reserved.
+ *  * // +----------------------------------------------------------------------
+ *  * // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  * // +----------------------------------------------------------------------
+ *  * // | Author: albert <Albert@rocareer.com>
+ *  * // +----------------------------------------------------------------------
+ *
+ */
+
+namespace app\api\controller;
+
+use plugin\radmin\service\auth\Auth;
+use Throwable;
+use plugin\radmin\extend\ba\Captcha;
+use plugin\radmin\extend\ba\ClickCaptcha;
+
+use app\common\controller\Frontend;
+use app\api\validate\User as UserValidate;
+
+class User extends Frontend
+{
+    protected array $noNeedLogin = ['checkIn', 'logout'];
+
+    protected array $noNeedPermission = ['index'];
+
+    public function initialize():void
+    {
+        parent::initialize();
+    }
+
+    /**
+     * 会员签入(登录和注册)
+     * @throws Throwable
+     */
+    public function checkIn()
+    {
+        $openMemberCenter = radmin_config('buildadmin.open_member_center');
+        if (!$openMemberCenter) {
+            return $this->error(__('Member center disabled'));
+        }
+
+        // 检查登录态
+        // if ($this->auth->isLogin()) {
+        //     return $this->success(__('You have already logged in. There is no need to log in again~'), [
+        //         'type' => $this->Member::LOGGED_IN
+        //     ], $this->Member::LOGIN_RESPONSE_CODE);
+        // }
+
+        $userLoginCaptchaSwitch = radmin_config('buildadmin.user_login_captcha');
+
+        if ($this->request->isPost()) {
+            $params = $this->request->only(['tab', 'email', 'mobile', 'username', 'password', 'keep', 'captcha', 'captchaId', 'captchaInfo', 'registerType']);
+
+            // 提前检查 tab ，然后将以 tab 值作为数据验证场景
+            if (!in_array($params['tab'] ?? '', ['login', 'register'])) {
+                return $this->error(__('Unknown operation'));
+            }
+
+            $validate = new UserValidate();
+            try {
+                $validate->scene($params['tab'])->check($params);
+            } catch (Throwable $e) {
+                return $this->error($e->getMessage());
+            }
+
+            if ($params['tab'] == 'login') {
+                if ($userLoginCaptchaSwitch) {
+                    $captchaObj = new ClickCaptcha();
+                    if (!$captchaObj->check($params['captchaId'], $params['captchaInfo'])) {
+                        return $this->error(__('Captcha error'));
+                    }
+                }
+                $captchaSwitch = radmin_config('buildadmin.user_login_captcha');
+                $credentials = [
+                    'username'      => $this->request->post('username'),
+                    'password'      => $this->request->post('password'),
+                    'captchaId'     => $this->request->post('captchaId'),
+                    'captchaInfo'   => $this->request->post('captchaInfo'),
+                    'captchaSwitch' => $captchaSwitch,
+                ];
+
+                $res = Member::login($credentials,'user', !empty($params['keep']));
+
+
+            } elseif ($params['tab'] == 'register') {
+                $captchaObj = new Captcha();
+                if (!$captchaObj->check($params['captcha'], $params[$params['registerType']] . 'user_register')) {
+                    return $this->error(__('Please enter the correct verification code'));
+                }
+                $res = $this->auth->register($params['username'], $params['password'], $params['mobile'], $params['email']);
+            }
+
+            if ($res) {
+                return $this->success(__('Login succeeded!'), [
+                    'userInfo'  => $res,
+                    'routePath' => '/user'
+                ]);
+            } else {
+
+                $msg =  __('Check in failed, please try again or contact the website administrator~');
+                return $this->error($msg);
+            }
+        }
+
+        return $this->success('', [
+            'userLoginCaptchaSwitch'  => $userLoginCaptchaSwitch,
+            'accountVerificationType' => get_account_verification_type()
+        ]);
+    }
+
+    public function logout()
+    {
+        if ($this->request->isPost()) {
+            $refreshToken = $this->request->post('refreshToken', '');
+            if ($refreshToken) Token::delete((string)$refreshToken);
+            $this->auth->logout();
+            return $this->success();
+        }
+    }
+}
---
+++ app/api/lang/en.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ *
+ *  * // +----------------------------------------------------------------------
+ *  * // | Rocareer [ ROC YOUR CAREER ]
+ *  * // +----------------------------------------------------------------------
+ *  * // | Copyright (c) 2014~2025 Albert@rocareer.com All rights reserved.
+ *  * // +----------------------------------------------------------------------
+ *  * // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  * // +----------------------------------------------------------------------
+ *  * // | Author: albert <Albert@rocareer.com>
+ *  * // +----------------------------------------------------------------------
+ *
+ */
+
+return [
+    'Invalid token'                                          => 'Invalid Token!',
+    'Login expired, please login again.'                     => 'Login expired, please login again.',
+    'Account not exist'                                      => 'Account does not exist',
+    'Account disabled'                                       => 'Account is disabled',
+    'Token login failed'                                     => 'Token login failed',
+    'Please try again after 1 day'                           => 'The number of failed login attempts has exceeded the limit, please try again after 24 hours.',
+    'Password is incorrect'                                  => 'Incorrect password',
+    'You are not logged in'                                  => 'You are not logged in.',
+    'Unknown operation'                                      => 'Unknown operation',
+    'No action available, please contact the administrator~' => 'There is no action available, please contact the administrator~',
+    'Please login first'                                     => 'Please login first！',
+    'You have no permission'                                 => 'No permission to operate！',
+    'Captcha error'                                          => 'Captcha error!',
+];
