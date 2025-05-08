98d113ae3063ac6e094c73a609ee43f2d48200d1
albert <albert@rocareer.com>
2025-05-09 02:25:11 +0800

    feat(rocareer): 新增蜘蛛管理相关服务与门面类 新增Spider、SpiderService、W、WorkerService类，实现蜘蛛处理、进程管理及工作服务功能
    Signed-off-by: albert <albert@rocareer.com>

---
+++ support/rocareer/W.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ *
+ *  * // +----------------------------------------------------------------------
+ *  * // | Rocareer [ ROC YOUR CAREER ]
+ *  * // +----------------------------------------------------------------------
+ *  * // | Copyright (c) 2014~2025 Albert@rocareer.com All rights reserved.
+ *  * // +----------------------------------------------------------------------
+ *  * // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  * // +----------------------------------------------------------------------
+ *  * // | Author: albert <Albert@rocareer.com>
+ *  * // +----------------------------------------------------------------------
+ *
+ */
+
+namespace plugin\radmin\support\rocareer;
+
+
+use app\common\library\token\driver;
+use think\Facade;
+
+/**
+ * Token 门面类
+ * @see Driver
+ * @method  onStart(int $id , int $pid)
+ * @method string getWorkerName(int $id)
+ */
+class W extends Facade
+{
+    protected static function getFacadeClass(): string
+    {
+        return WorkerService::class;
+    }
+}
