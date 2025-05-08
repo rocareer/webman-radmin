ddee6769bc0e3114ab7f4d6ef725060d689db2fa
albert <albert@rocareer.com>
2025-05-09 01:53:24 +0800

    新增账号管理相关API接口，包含账户概览、资料编辑、验证与绑定、密码修改、积分与余额日志查询及找回密码功能，同时新增Ajax文件上传、图形验证码等通用接口，并支持邮件发送、安装向导等功能
    Signed-off-by: albert <albert@rocareer.com>

---
+++ app/api/controller/Common.php
@@ -0,0 +1,92 @@
+<?php
+/*
+ *
+ *  * // +----------------------------------------------------------------------
+ *  * // | Rocareer [ ROC YOUR CAREER ]
+ *  * // +----------------------------------------------------------------------
+ *  * // | Copyright (c) 2014~2025 Albert@rocareer.com All rights reserved.
+ *  * // +----------------------------------------------------------------------
+ *  * // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  * // +----------------------------------------------------------------------
+ *  * // | Author: albert <Albert@rocareer.com>
+ *  * // +----------------------------------------------------------------------
+ *
+ */
+
+namespace app\api\controller;
+
+use app\common\controller\Api;
+use plugin\radmin\extend\ba\Captcha;
+use plugin\radmin\extend\ba\ClickCaptcha;
+use plugin\radmin\support\StatusCode;
+use plugin\radmin\support\token\Token;
+use Rocareer\Radmin\Exception\BusinessException;
+use support\Response;
+use Throwable;
+
+
+class Common extends Api
+{
+    /**
+     * 图形验证码
+     * @throws Throwable
+     */
+    public function captcha(): Response
+    {
+        $captchaId = $this->request->input('id');
+        $config    = array(
+            'codeSet'  => '123456789',            // 验证码字符集合
+            'fontSize' => 22,                     // 验证码字体大小(px)
+            'useCurve' => false,                  // 是否画混淆曲线
+            'useNoise' => true,                   // 是否添加杂点
+            'length'   => 4,                      // 验证码位数
+            'bg'       => array(255, 255, 255),   // 背景颜色
+        );
+
+        $captcha = new Captcha($config);
+        return $captcha->entry($captchaId);
+    }
+
+    /**
+     * 点选验证码
+     */
+    public function clickCaptcha()
+    {
+        $id      = $this->request->input('id');
+        $captcha = new ClickCaptcha();
+        return $this->success('', $captcha->creat($id));
+    }
+
+    /**
+     * 点选验证码检查
+     * @throws Throwable
+     */
+    public function checkClickCaptcha()
+    {
+        $id      = $this->request->post('id');
+        $info    = $this->request->post('info');
+        $unset   = $this->request->post('unset', false);
+        $captcha = new ClickCaptcha();
+        if ($captcha->check($id, $info, $unset)) return $this->success();
+        return $this->error();
+    }
+
+    public function refreshToken()
+    {
+        $refreshToken = $this->request->post('refreshToken');
+        if (empty($refreshToken)){
+            throw new BusinessException('凭证已失效:请重新登录 ',StatusCode::NEED_LOGIN, true);
+        }
+        try {
+            $payload = Token::verify($refreshToken);
+        } catch (\Exception $e) {
+            throw new BusinessException('凭证已失效:请重新登录 ', StatusCode::NEED_LOGIN, true);
+        }
+
+        $newToken= Token::refresh($refreshToken);
+        return $this->success('', [
+            'type'  => $payload->type,
+            'token' => $newToken
+        ]);
+    }
+}
---
+++ app/api/controller/Index.php
@@ -0,0 +1,113 @@
+<?php
+/*
+ *
+ *  * // +----------------------------------------------------------------------
+ *  * // | Rocareer [ ROC YOUR CAREER ]
+ *  * // +----------------------------------------------------------------------
+ *  * // | Copyright (c) 2014~2025 Albert@rocareer.com All rights reserved.
+ *  * // +----------------------------------------------------------------------
+ *  * // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  * // +----------------------------------------------------------------------
+ *  * // | Author: albert <Albert@rocareer.com>
+ *  * // +----------------------------------------------------------------------
+ *
+ */
+
+namespace app\api\controller;
+
+use app\common\controller\Frontend;
+use plugin\radmin\extend\ba\Tree;
+use plugin\radmin\service\auth\admin\AdminHelper;
+use plugin\radmin\support\member\Member;
+use support\think\Db;
+use Throwable;
+
+class Index extends Frontend
+{
+    protected array $noNeedLogin = ['index'];
+
+    public function initialize():void
+    {
+        parent::initialize();
+    }
+
+    /**
+     * 前台和会员中心的初始化请求
+     *
+     * @throws Throwable
+     */
+    public function index()
+    {
+        $menus = [];
+
+        if ($this->request->adminIsLogin) {
+            $rules     = [];
+            $userMenus = AdminHelper::getMenus();
+
+            // 首页加载的规则，验权，但过滤掉会员中心菜单
+            foreach ($userMenus as $item) {
+                if ($item['type'] == 'menu_dir') {
+                    $menus[] = $item;
+                } elseif ($item['type'] != 'menu') {
+                    $rules[] = $item;
+                }
+            }
+        } else {
+            // 若是从前台会员中心内发出的请求，要求必须登录，否则会员中心异常
+
+            $userInfo=[];
+            if (Member::isLogin()){
+                $userInfo=Member::getUserInfo($this->request->auth->user->id);
+            }
+            $requiredLogin = $this->request->get('requiredLogin', false);
+            if ($requiredLogin) {
+
+                // 触发可能的 token 过期异常
+
+                //todo 这里检查 token 是否过期
+
+                //				return $this->error(__('Please login first'), [
+                //					'type' => $this->Member::NEED_LOGIN,
+                //				], $this->Member::LOGIN_RESPONSE_CODE);
+            }
+
+            $rules         = Db::name('user_rule')
+                ->where('status', '1')
+                ->where('no_login_valid', 1)
+                ->where('type', 'in', [
+                    'route',
+                    'nav',
+                    'button',
+                ])
+                ->order('weigh', 'desc')
+                ->select()
+                ->toArray();
+            $rules         = Tree::instance()
+                ->assembleChild($rules);
+            $data['rules'] = array_values($rules);
+        }
+
+
+        $data = [
+            'site'             => [
+                'siteName'     => get_sys_config('site_name'),
+                'recordNumber' => get_sys_config('record_number'),
+                'version'      => get_sys_config('version'),
+                'cdnUrl'       => full_url(),
+                'upload'       => keys_to_camel_case(get_upload_config(), [
+                    'max_size',
+                    'save_name',
+                    'allowed_suffixes',
+                    'allowed_mime_types',
+                ]),
+            ],
+            'openMemberCenter' => config('plugin.radmin.buildadmin.open_member_center'),
+            'userInfo'         => $userInfo,
+            'menus'            => $menus,
+            'rules'            => array_values($rules),
+
+        ];
+
+        return $this->success('Init', $data);
+    }
+}
