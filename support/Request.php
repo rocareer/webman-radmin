d1189fd24a334b94d4b44e77234b545eb2ed086e
albert <albert@rocareer.com>
2025-05-09 19:28:00 +0800

    feat(Request): 新增 controllerName 和 role 属性
    Signed-off-by: albert <albert@rocareer.com>

--- support/Request.php
+++ support/Request.php
@@ -10,9 +10,15 @@ namespace support;
  */
 class Request extends \Webman\Http\Request
 {
-    public string|null $token  = null;
-    public object|null $member = null;
-    public array|null  $roles  = null;
-    public mixed       $upload = null;
+    /**
+     * @var array|mixed|null
+     */
+
+    public string|null $token          = null;
+    public object|null $member         = null;
+    public array|null  $roles          = null;
+    public mixed       $upload         = null;
+    public string|null $controllerName = null;
+    public string|null $role           = null;
 
 }
