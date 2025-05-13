9b58597cfa805a0c0e258718e16d0f21c1dda039
albert <albert@rocareer.com>
2025-05-14 03:06:57 +0800

    feat(middleware): 记录同步操作日志 在请求方法为 POST、DELETE 或操作名为 sync 时记录管理员操作日志
    Signed-off-by: albert <albert@rocareer.com>

--- app/middleware/AdminLog.php
+++ app/middleware/AdminLog.php
@@ -30,7 +30,7 @@ class AdminLog implements MiddlewareInterface
 	public function process(Request $request, callable $handler): Response
     {
         $response = $handler($request);
-        if (($request->isPost() || $request->method()=='DELETE') &&  config('buildadmin.auto_write_admin_log')) AdminLogModel::instance()->record();
+        if (($request->isPost() || $request->method()=='DELETE'||$request->action=='sync') &&  config('buildadmin.auto_write_admin_log')) AdminLogModel::instance()->record();
         return $response;
     }
 }
