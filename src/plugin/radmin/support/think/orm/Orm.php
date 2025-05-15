0ea78df81bf3303e48375347e5179fe574518cb1
albert <albert@rocareer.com>
2025-05-16 00:06:40 +0800

    feat(radmin-orm): 新增ThinkORM支持插件 引入Rdb、Rdbm、Model等类，实现数据库连接池管理和事务处理，增强数据库操作能力
    Signed-off-by: albert <albert@rocareer.com>

---
+++ src/plugin/radmin/support/think/orm/Orm.php
@@ -0,0 +1,59 @@
+<?php
+
+
+namespace plugin\radmin\support\think\orm;
+
+use support\think\Cache;
+use think\Container;
+use think\Paginator;
+use Webman\Bootstrap;
+
+class Orm implements Bootstrap
+{
+	/**
+	 * @var bool
+	 */
+	private static bool $initialized = false;
+	
+	/**
+	 
+	 */
+	public static function start($worker)
+	{
+		if (self::$initialized) {
+			return;
+		}
+		self::$initialized = true;
+		
+		$config = config('plugin.radmin.think-orm', config('plugin.radmin.thinkorm'));
+		if (!$config) {
+			return;
+		}
+		Container::getInstance()->bind('Rdbm', Rdbm::class);
+
+		// 配置
+		Rdb::setConfig($config);
+
+		if (class_exists(Cache::class)) {
+			Rdb::setCache(Cache::store());
+		}
+		
+		Paginator::currentPageResolver(function ($pageName = 'page') {
+			$request = request();
+			if (!$request) {
+				return 1;
+			}
+			$page = $request->input($pageName, 1);
+			if (filter_var($page, FILTER_VALIDATE_INT) !== false && (int)$page >= 1) {
+				return (int)$page;
+			}
+			return 1;
+		});
+		
+		// 设置分页url中域名与参数之间的path字符串
+		Paginator::currentPathResolver(function () {
+			$request = request();
+			return $request ? $request->path() : '/';
+		});
+	}
+}
