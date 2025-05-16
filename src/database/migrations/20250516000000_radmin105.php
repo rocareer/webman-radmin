c63afbaf0109eb4dc26f181cf1b78681a3d1772f
albert <albert@rocareer.com>
2025-05-16 22:11:51 +0800

    chore(migration): 添加数据管理相关表及菜单规则迁移
    本次提交包含三个迁移文件：
    1. 20250515151515_radmin104.php - 添加鉴权配置项和系统配置
    2. 20250516000000_radmin105.php - 添加管理员菜单规则
    3. 20250517000000_radmin106.php - 创建数据备份和日志相关表

---
+++ src/database/migrations/20250516000000_radmin105.php
@@ -0,0 +1,498 @@
+<?php
+/**
+ * File:        20250516000000_radmin105.php
+ * Author:      albert <albert@rocareer.com>
+ * Created:     2025/5/16 00:00
+ * Description: v105 数据升级 - 添加菜单规则
+ *
+ * Copyright [2014-2026] [https://rocareer.com]
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ */
+
+use Phinx\Migration\AbstractMigration;
+use plugin\radmin\support\Log;
+
+class Radmin105 extends AbstractMigration
+{
+    /**
+     * @throws Throwable
+     */
+    public function up()
+    {
+        $this->insertAdminRules();
+    }
+
+    /**
+     * 插入管理员规则数据
+     * @return void
+     * @throws \Exception
+     * Author:   albert <albert@rocareer.com>
+     * Time:     2025/5/16 00:00
+     */
+    public function insertAdminRules()
+    {
+        // 获取表对象
+        $table = $this->table('ra_admin_rule');
+
+        // 准备要插入的数据
+        $data = [
+            [
+                'id' => 132,
+                'pid' => 55,
+                'type' => 'menu_dir',
+                'title' => '数据库备份',
+                'name' => 'security/backup',
+                'path' => 'security/backup',
+                'icon' => 'fa fa-upload',
+                'menu_type' => null,
+                'url' => '',
+                'component' => '',
+                'keepalive' => 0,
+                'extend' => 'none',
+                'remark' => '',
+                'weigh' => 0,
+                'status' => '1',
+                'update_time' => 1747048032,
+                'create_time' => 1747047140
+            ],
+            [
+                'id' => 146,
+                'pid' => 0,
+                'type' => 'menu_dir',
+                'title' => '数据管理',
+                'name' => 'data',
+                'path' => '',
+                'icon' => 'fa fa-database',
+                'menu_type' => 'tab',
+                'url' => '',
+                'component' => '',
+                'keepalive' => 0,
+                'extend' => 'none',
+                'remark' => '',
+                'weigh' => 0,
+                'status' => '1',
+                'update_time' => 1747050863,
+                'create_time' => 1747050813
+            ],
+            [
+                'id' => 160,
+                'pid' => 146,
+                'type' => 'menu',
+                'title' => '数据备份',
+                'name' => 'data/backup',
+                'path' => 'data/backup',
+                'icon' => '',
+                'menu_type' => 'tab',
+                'url' => '',
+                'component' => '/src/views/backend/data/backup/index.vue',
+                'keepalive' => 1,
+                'extend' => 'none',
+                'remark' => '',
+                'weigh' => 0,
+                'status' => '1',
+                'update_time' => 1747063292,
+                'create_time' => 1747051055
+            ],
+            [
+                'id' => 161,
+                'pid' => 160,
+                'type' => 'button',
+                'title' => '查看',
+                'name' => 'data/backup/index',
+                'path' => '',
+                'icon' => '',
+                'menu_type' => null,
+                'url' => '',
+                'component' => '',
+                'keepalive' => 0,
+                'extend' => 'none',
+                'remark' => '',
+                'weigh' => 0,
+                'status' => '1',
+                'update_time' => 1747063377,
+                'create_time' => 1747051055
+            ],
+            [
+                'id' => 162,
+                'pid' => 160,
+                'type' => 'button',
+                'title' => '添加',
+                'name' => 'data/backup/add',
+                'path' => '',
+                'icon' => '',
+                'menu_type' => null,
+                'url' => '',
+                'component' => '',
+                'keepalive' => 0,
+                'extend' => 'none',
+                'remark' => '',
+                'weigh' => 0,
+                'status' => '1',
+                'update_time' => 1747063359,
+                'create_time' => 1747051055
+            ],
+            [
+                'id' => 163,
+                'pid' => 160,
+                'type' => 'button',
+                'title' => '编辑',
+                'name' => 'data/backup/edit',
+                'path' => '',
+                'icon' => '',
+                'menu_type' => null,
+                'url' => '',
+                'component' => '',
+                'keepalive' => 0,
+                'extend' => 'none',
+                'remark' => '',
+                'weigh' => 0,
+                'status' => '1',
+                'update_time' => 1747063344,
+                'create_time' => 1747051055
+            ],
+            [
+                'id' => 164,
+                'pid' => 160,
+                'type' => 'button',
+                'title' => '删除',
+                'name' => 'data/backup/del',
+                'path' => '',
+                'icon' => '',
+                'menu_type' => null,
+                'url' => '',
+                'component' => '',
+                'keepalive' => 0,
+                'extend' => 'none',
+                'remark' => '',
+                'weigh' => 0,
+                'status' => '1',
+                'update_time' => 1747063328,
+                'create_time' => 1747051055
+            ],
+            [
+                'id' => 165,
+                'pid' => 160,
+                'type' => 'button',
+                'title' => '快速排序',
+                'name' => 'data/backup/sortable',
+                'path' => '',
+                'icon' => '',
+                'menu_type' => null,
+                'url' => '',
+                'component' => '',
+                'keepalive' => 0,
+                'extend' => 'none',
+                'remark' => '',
+                'weigh' => 0,
+                'status' => '1',
+                'update_time' => 1747063314,
+                'create_time' => 1747051055
+            ],
+            [
+                'id' => 166,
+                'pid' => 0,
+                'type' => 'menu_dir',
+                'title' => '日志管理',
+                'name' => 'log',
+                'path' => 'log',
+                'icon' => 'el-icon-InfoFilled',
+                'menu_type' => null,
+                'url' => '',
+                'component' => '',
+                'keepalive' => 0,
+                'extend' => 'none',
+                'remark' => '',
+                'weigh' => 0,
+                'status' => '1',
+                'update_time' => 1747051833,
+                'create_time' => 1747051767
+            ],
+            [
+                'id' => 167,
+                'pid' => 166,
+                'type' => 'menu_dir',
+                'title' => 'login',
+                'name' => 'login',
+                'path' => 'login',
+                'icon' => '',
+                'menu_type' => null,
+                'url' => '',
+                'component' => '',
+                'keepalive' => 0,
+                'extend' => 'none',
+                'remark' => '',
+                'weigh' => 0,
+                'status' => '1',
+                'update_time' => 1747051767,
+                'create_time' => 1747051767
+            ],
+            [
+                'id' => 168,
+                'pid' => 167,
+                'type' => 'menu',
+                'title' => '管理员登录',
+                'name' => 'log/login/admin',
+                'path' => 'log/login/admin',
+                'icon' => '',
+                'menu_type' => 'tab',
+                'url' => '',
+                'component' => '/src/views/backend/log/login/admin/index.vue',
+                'keepalive' => 1,
+                'extend' => 'none',
+                'remark' => '',
+                'weigh' => 0,
+                'status' => '1',
+                'update_time' => 1747051767,
+                'create_time' => 1747051767
+            ],
+            [
+                'id' => 169,
+                'pid' => 168,
+                'type' => 'button',
+                'title' => '查看',
+                'name' => 'log/login/admin/index',
+                'path' => '',
+                'icon' => '',
+                'menu_type' => null,
+                'url' => '',
+                'component' => '',
+                'keepalive' => 0,
+                'extend' => 'none',
+                'remark' => '',
+                'weigh' => 0,
+                'status' => '1',
+                'update_time' => 1747051767,
+                'create_time' => 1747051767
+            ],
+            [
+                'id' => 170,
+                'pid' => 168,
+                'type' => 'button',
+                'title' => '添加',
+                'name' => 'log/login/admin/add',
+                'path' => '',
+                'icon' => '',
+                'menu_type' => null,
+                'url' => '',
+                'component' => '',
+                'keepalive' => 0,
+                'extend' => 'none',
+                'remark' => '',
+                'weigh' => 0,
+                'status' => '1',
+                'update_time' => 1747051767,
+                'create_time' => 1747051767
+            ],
+            [
+                'id' => 171,
+                'pid' => 168,
+                'type' => 'button',
+                'title' => '编辑',
+                'name' => 'log/login/admin/edit',
+                'path' => '',
+                'icon' => '',
+                'menu_type' => null,
+                'url' => '',
+                'component' => '',
+                'keepalive' => 0,
+                'extend' => 'none',
+                'remark' => '',
+                'weigh' => 0,
+                'status' => '1',
+                'update_time' => 1747051767,
+                'create_time' => 1747051767
+            ],
+            [
+                'id' => 172,
+                'pid' => 168,
+                'type' => 'button',
+                'title' => '删除',
+                'name' => 'log/login/admin/del',
+                'path' => '',
+                'icon' => '',
+                'menu_type' => null,
+                'url' => '',
+                'component' => '',
+                'keepalive' => 0,
+                'extend' => 'none',
+                'remark' => '',
+                'weigh' => 0,
+                'status' => '1',
+                'update_time' => 1747051767,
+                'create_time' => 1747051767
+            ],
+            [
+                'id' => 173,
+                'pid' => 168,
+                'type' => 'button',
+                'title' => '快速排序',
+                'name' => 'log/login/admin/sortable',
+                'path' => '',
+                'icon' => '',
+                'menu_type' => null,
+                'url' => '',
+                'component' => '',
+                'keepalive' => 0,
+                'extend' => 'none',
+                'remark' => '',
+                'weigh' => 0,
+                'status' => '1',
+                'update_time' => 1747051767,
+                'create_time' => 1747051767
+            ],
+            [
+                'id' => 175,
+                'pid' => 146,
+                'type' => 'menu',
+                'title' => '数据表',
+                'name' => 'data/table',
+                'path' => 'data/table',
+                'icon' => '',
+                'menu_type' => 'tab',
+                'url' => '',
+                'component' => '/src/views/backend/data/table/index.vue',
+                'keepalive' => 1,
+                'extend' => 'none',
+                'remark' => '',
+                'weigh' => 0,
+                'status' => '1',
+                'update_time' => 1747062437,
+                'create_time' => 1747053457
+            ],
+            [
+                'id' => 176,
+                'pid' => 175,
+                'type' => 'button',
+                'title' => '查看',
+                'name' => 'data/table/index',
+                'path' => '',
+                'icon' => '',
+                'menu_type' => null,
+                'url' => '',
+                'component' => '',
+                'keepalive' => 0,
+                'extend' => 'none',
+                'remark' => '',
+                'weigh' => 0,
+                'status' => '1',
+                'update_time' => 1747062307,
+                'create_time' => 1747053457
+            ],
+            [
+                'id' => 177,
+                'pid' => 175,
+                'type' => 'button',
+                'title' => '添加',
+                'name' => 'data/table/add',
+                'path' => '',
+                'icon' => '',
+                'menu_type' => null,
+                'url' => '',
+                'component' => '',
+                'keepalive' => 0,
+                'extend' => 'none',
+                'remark' => '',
+                'weigh' => 0,
+                'status' => '1',
+                'update_time' => 1747062359,
+                'create_time' => 1747053457
+            ],
+            [
+                'id' => 178,
+                'pid' => 175,
+                'type' => 'button',
+                'title' => '编辑',
+                'name' => 'data/table/edit',
+                'path' => '',
+                'icon' => '',
+                'menu_type' => null,
+                'url' => '',
+                'component' => '',
+                'keepalive' => 0,
+                'extend' => 'none',
+                'remark' => '',
+                'weigh' => 0,
+                'status' => '1',
+                'update_time' => 1747062390,
+                'create_time' => 1747053457
+            ],
+            [
+                'id' => 179,
+                'pid' => 175,
+                'type' => 'button',
+                'title' => '删除',
+                'name' => 'data/table/del',
+                'path' => '',
+                'icon' => '',
+                'menu_type' => null,
+                'url' => '',
+                'component' => '',
+                'keepalive' => 0,
+                'extend' => 'none',
+                'remark' => '',
+                'weigh' => 0,
+                'status' => '1',
+                'update_time' => 1747062373,
+                'create_time' => 1747053457
+            ],
+            [
+                'id' => 180,
+                'pid' => 175,
+                'type' => 'button',
+                'title' => '快速排序',
+                'name' => 'data/table/sortable',
+                'path' => '',
+                'icon' => '',
+                'menu_type' => null,
+                'url' => '',
+                'component' => '',
+                'keepalive' => 0,
+                'extend' => 'none',
+                'remark' => '',
+                'weigh' => 0,
+                'status' => '1',
+                'update_time' => 1747062342,
+                'create_time' => 1747053457
+            ],
+            [
+                'id' => 181,
+                'pid' => 175,
+                'type' => 'button',
+                'title' => '同步',
+                'name' => 'data/table/sync',
+                'path' => '',
+                'icon' => 'fa fa-circle-o',
+                'menu_type' => 'tab',
+                'url' => '',
+                'component' => '',
+                'keepalive' => 0,
+                'extend' => 'none',
+                'remark' => '',
+                'weigh' => 0,
+                'status' => '1',
+                'update_time' => 1747062322,
+                'create_time' => 1747061573
+            ],
+        ];
+
+        try {
+            // 检查每条记录是否已存在，如果不存在则插入
+            foreach ($data as $item) {
+                $id = $item['id'];
+                $exists = $this->fetchRow("SELECT id FROM ra_admin_rule WHERE id = $id");
+
+                if (!$exists) {
+                    $table->insert($item)->save();
+                    Log::info('插入管理员规则', ['id' => $id, 'title' => $item['title']]);
+                } else {
+                    Log::info('管理员规则已存在', ['id' => $id, 'title' => $item['title']]);
+                }
+            }
+
+            Log::info('管理员规则数据迁移完成');
+        } catch (\Exception $e) {
+            Log::error('管理员规则数据迁移失败', ['error' => $e->getMessage()]);
+            throw $e;
+        }
+    }
+}
---
+++ src/database/migrations/20250517000000_radmin106.php
@@ -0,0 +1,258 @@
+<?php
+/**
+ * File:        20250517000000_radmin106.php
+ * Author:      albert <albert@rocareer.com>
+ * Created:     2025/5/17 00:00
+ * Description: v106 数据升级 - 创建数据管理相关表
+ *
+ * Copyright [2014-2026] [https://rocareer.com]
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ */
+
+use Phinx\Migration\AbstractMigration;
+use plugin\radmin\support\Log;
+
+class Radmin106 extends AbstractMigration
+{
+    /**
+     * @throws Throwable
+     */
+    public function up()
+    {
+        $this->createDataBackupTable();
+        $this->createDataBackupLogTable();
+        $this->createDataTableTable();
+    }
+
+    /**
+     * 创建数据备份表
+     * @return void
+     * @throws \Exception
+     * Author:   albert <albert@rocareer.com>
+     * Time:     2025/5/17 00:00
+     */
+    public function createDataBackupTable()
+    {
+        $tableName = 'ra_data_backup';
+
+        // 检查表是否已存在
+        if ($this->hasTable($tableName)) {
+            Log::info('表已存在，跳过创建', ['table' => $tableName]);
+            return;
+        }
+
+        try {
+            $table = $this->table($tableName, [
+                'id' => false,
+                'primary_key' => ['id'],
+                'engine' => 'InnoDB',
+                'encoding' => 'utf8mb4',
+                'collation' => 'utf8mb4_unicode_ci',
+                'comment' => '数据备份',
+                'row_format' => 'DYNAMIC'
+            ]);
+
+            $table->addColumn('id', 'integer', [
+                'signed' => false,
+                'identity' => true,
+                'comment' => 'ID'
+            ])
+            ->addColumn('table_name', 'string', [
+                'null' => true,
+                'default' => '',
+                'limit' => 255,
+                'comment' => '远程下拉'
+            ])
+            ->addColumn('version', 'string', [
+                'null' => true,
+                'default' => '',
+                'limit' => 255,
+                'comment' => '版本号'
+            ])
+            ->addColumn('file', 'string', [
+                'null' => false,
+                'default' => '',
+                'limit' => 255,
+                'comment' => '备份文件'
+            ])
+            ->addColumn('remark', 'string', [
+                'null' => false,
+                'default' => '',
+                'limit' => 255,
+                'comment' => '备注'
+            ])
+            ->addColumn('status', 'integer', [
+                'signed' => false,
+                'null' => false,
+                'default' => 0,
+                'limit' => 1,
+                'comment' => '状态:0=未开始,1=成功,2=失败'
+            ])
+            ->addColumn('runnow', 'integer', [
+                'signed' => false,
+                'null' => false,
+                'default' => 1,
+                'limit' => 1,
+                'comment' => '立即开始:0=否,1=是'
+            ])
+            ->addColumn('run_time', 'integer', [
+                'signed' => false,
+                'null' => true,
+                'limit' => 20,
+                'comment' => '执行时间'
+            ])
+            ->addColumn('create_time', 'integer', [
+                'signed' => false,
+                'null' => true,
+                'limit' => 20,
+                'comment' => '创建时间'
+            ])
+            ->create();
+
+            Log::info('创建数据备份表成功', ['table' => $tableName]);
+        } catch (\Exception $e) {
+            Log::error('创建数据备份表失败', ['table' => $tableName, 'error' => $e->getMessage()]);
+            throw $e;
+        }
+    }
+
+    /**
+     * 创建数据备份日志表
+     * @return void
+     * @throws \Exception
+     * Author:   albert <albert@rocareer.com>
+     * Time:     2025/5/17 00:00
+     */
+    public function createDataBackupLogTable()
+    {
+        $tableName = 'ra_data_backup_log';
+
+        // 检查表是否已存在
+        if ($this->hasTable($tableName)) {
+            Log::info('表已存在，跳过创建', ['table' => $tableName]);
+            return;
+        }
+
+        try {
+            $table = $this->table($tableName, [
+                'id' => false,
+                'primary_key' => ['id'],
+                'engine' => 'InnoDB',
+                'encoding' => 'utf8mb4',
+                'collation' => 'utf8mb4_unicode_ci',
+                'comment' => '操作日志',
+                'row_format' => 'DYNAMIC'
+            ]);
+
+            $table->addColumn('id', 'integer', [
+                'signed' => false,
+                'identity' => true,
+                'comment' => 'ID'
+            ])
+            ->addColumn('string', 'string', [
+                'null' => false,
+                'default' => '',
+                'limit' => 255,
+                'comment' => '字符串'
+            ])
+            ->addColumn('backup_id', 'integer', [
+                'signed' => false,
+                'null' => true,
+                'comment' => '远程下拉'
+            ])
+            ->addColumn('create_time', 'integer', [
+                'signed' => false,
+                'null' => true,
+                'limit' => 20,
+                'comment' => '创建时间'
+            ])
+            ->create();
+
+            Log::info('创建数据备份日志表成功', ['table' => $tableName]);
+        } catch (\Exception $e) {
+            Log::error('创建数据备份日志表失败', ['table' => $tableName, 'error' => $e->getMessage()]);
+            throw $e;
+        }
+    }
+
+    /**
+     * 创建数据表信息表
+     * @return void
+     * @throws \Exception
+     * Author:   albert <albert@rocareer.com>
+     * Time:     2025/5/17 00:00
+     */
+    public function createDataTableTable()
+    {
+        $tableName = 'ra_data_table';
+
+        // 检查表是否已存在
+        if ($this->hasTable($tableName)) {
+            Log::info('表已存在，跳过创建', ['table' => $tableName]);
+            return;
+        }
+
+        try {
+            $table = $this->table($tableName, [
+                'id' => false,
+                'primary_key' => ['id'],
+                'engine' => 'InnoDB',
+                'encoding' => 'utf8mb4',
+                'collation' => 'utf8mb4_unicode_ci',
+                'comment' => '数据表',
+                'row_format' => 'DYNAMIC'
+            ]);
+
+            $table->addColumn('id', 'integer', [
+                'signed' => false,
+                'identity' => true,
+                'comment' => 'ID'
+            ])
+            ->addColumn('name', 'string', [
+                'null' => false,
+                'default' => '',
+                'limit' => 255,
+                'comment' => '表名'
+            ])
+            ->addColumn('charset', 'string', [
+                'null' => false,
+                'default' => '',
+                'limit' => 255,
+                'comment' => '字符集'
+            ])
+            ->addColumn('record_count', 'integer', [
+                'null' => true,
+                'comment' => '记录数'
+            ])
+            ->addColumn('comment', 'string', [
+                'null' => true,
+                'limit' => 255,
+                'comment' => '描述'
+            ])
+            ->addColumn('engine', 'string', [
+                'null' => false,
+                'default' => '',
+                'limit' => 255,
+                'comment' => '引擎'
+            ])
+            ->addColumn('remark', 'string', [
+                'null' => false,
+                'default' => '',
+                'limit' => 255,
+                'comment' => '备注'
+            ])
+            ->addColumn('create_time', 'integer', [
+                'signed' => false,
+                'null' => true,
+                'limit' => 20,
+                'comment' => '创建时间'
+            ])
+            ->create();
+
+            Log::info('创建数据表信息表成功', ['table' => $tableName]);
+        } catch (\Exception $e) {
+            Log::error('创建数据表信息表失败', ['table' => $tableName, 'error' => $e->getMessage()]);
+            throw $e;
+        }
+    }
+}
