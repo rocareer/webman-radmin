9877be4db716f37c9051e6749bf26136bd457ade
albert <albert@rocareer.com>
2025-05-18 23:53:05 +0800

    新增:composer 函数载入

--- src/HelperFile.php
+++ src/HelperFile.php
@@ -64,7 +64,6 @@ class HelperFile
     /**
      * 删除一个路径下的所有相对空文件夹（删除此路径中的所有空文件夹）
      * @param string $path 相对于根目录的文件夹路径 如`c:BuildAdmin/a/b/`
-     
      */
     public static function delEmptyDir(string $path)
     {
@@ -267,8 +266,8 @@ class HelperFile
      * Author:   albert <albert@rocareer.com>
      * Time:     2025/5/18 23:27
      */
-   public static function copyDir($source, $destination): void
-   {
+    public static function copyDir($source, $destination): void
+    {
         // 检查源文件夹是否存在
         if (!is_dir($source)) {
             throw new Exception("Source folder does not exist: $source");
@@ -289,7 +288,7 @@ class HelperFile
                 continue;
             }
 
-            $sourcePath = $source . DIRECTORY_SEPARATOR . $file;
+            $sourcePath      = $source . DIRECTORY_SEPARATOR . $file;
             $destinationPath = $destination . DIRECTORY_SEPARATOR . $file;
 
             // 如果是文件夹，递归拷贝
@@ -304,5 +303,81 @@ class HelperFile
         // 关闭文件夹
         closedir($dir);
     }
+
+    public static function syncDir($source, $destination): void
+    {
+        // 检查源文件夹是否存在
+        if (!is_dir($source)) {
+            throw new Exception("Source folder does not exist: $source");
+        }
+
+        // 创建临时目录
+        $tempDestination = $destination . '_temp';
+        if (!mkdir($tempDestination, 0755, true) && !is_dir($tempDestination)) {
+            throw new Exception("Failed to create temporary directory: $tempDestination");
+        }
+
+        try {
+            // 打开源文件夹
+            $dir = opendir($source);
+
+            // 遍历源文件夹中的每个文件和子文件夹
+            while (($file = readdir($dir)) !== false) {
+                // 跳过特殊文件夹 . 和 ..
+                if ($file == '.' || $file == '..') {
+                    continue;
+                }
+
+                $sourcePath = $source . DIRECTORY_SEPARATOR . $file;
+                $tempPath   = $tempDestination . DIRECTORY_SEPARATOR . $file;
+
+                // 如果是文件夹，递归拷贝
+                if (is_dir($sourcePath)) {
+                    self::syncDir($sourcePath, $tempPath);
+                } else {
+                    // 如果是文件，直接拷贝
+                    copy($sourcePath, $tempPath);
+                }
+            }
+
+            // 关闭文件夹
+            closedir($dir);
+
+            // 删除目标目录
+            self::deleteDir($destination);
+
+            // 将临时目录重命名为目标目录
+            rename($tempDestination, $destination);
+        } catch (Exception $e) {
+            // 如果发生异常，删除临时目录并还原目标目录
+            self::deleteDir($tempDestination);
+            throw $e; // 重新抛出异常
+        }
+    }
+
+    // 删除目录的辅助方法
+    private static function deleteDir($dir): void
+    {
+        if (!is_dir($dir)) {
+            return;
+        }
+
+        $items = scandir($dir);
+        foreach ($items as $item) {
+            if ($item == '.' || $item == '..') {
+                continue;
+            }
+
+            $itemPath = $dir . DIRECTORY_SEPARATOR . $item;
+            if (is_dir($itemPath)) {
+                self::deleteDir($itemPath);
+            } else {
+                unlink($itemPath);
+            }
+        }
+
+        rmdir($dir);
+    }
+
 
 }
